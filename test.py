609a42304dc2c4b99220f3a00550f5063813d778
Roman Steinberg <roman.accs@gmail.com>
2018-07-02 16:18:43 +0300

    feat(multiple test process): basic functionality
    add possibility to test several models corresponding to interval of epochs

--- test.py
+++ test.py
@@ -7,28 +7,46 @@ from util.visualizer import Visualizer
 from pdb import set_trace as st
 from util import html
 
-opt = TestOptions().parse()
-opt.nThreads = 1   # test code only supports nThreads = 1
-opt.batchSize = 1  # test code only supports batchSize = 1
-opt.serial_batches = True  # no shuffle
-opt.no_flip = True  # no flip
 
-data_loader = CreateDataLoader(opt)
-dataset = data_loader.load_data()
-model = create_model(opt)
-visualizer = Visualizer(opt)
-# create website
-web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
-webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
-# test
-for i, data in enumerate(dataset):
-    if i >= opt.how_many:
-        break
-    model.set_input(data)
-    model.test()
-    visuals = model.get_current_visuals()
-    img_path = model.get_image_paths()
-    print('process image... %s' % img_path)
-    visualizer.save_images(webpage, visuals, img_path)
+def main():
+    opt = TestOptions().parse()
+    opt.nThreads = 1   # test code only supports nThreads = 1
+    opt.batchSize = 1  # test code only supports batchSize = 1
+    opt.serial_batches = True  # no shuffle
+    opt.no_flip = True  # no flip
+    if ',' in opt.which_epoch:
+        range_str = list(map(int, str(opt.which_epoch).split(',')))
+        first_epoch, last_epoch = range_str[:2]
+        step = range_str[2] if len(range_str) == 3 else 1
+        res_dir = opt.results_dir
+        for epoch in range(first_epoch, last_epoch, step):
+            opt.which_epoch = epoch
+            opt.results_dir = os.path.join(res_dir, 'epoch_%d' % epoch)
+            test(opt)
+    else:
+        test(opt)
 
-webpage.save()
+
+def test(opt):
+    data_loader = CreateDataLoader(opt)
+    dataset = data_loader.load_data()
+    model = create_model(opt)
+    visualizer = Visualizer(opt)
+    # create website
+    web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+    # test
+    for i, data in enumerate(dataset):
+        if i >= opt.how_many:
+            break
+        model.set_input(data)
+        model.test()
+        visuals = model.get_current_visuals()
+        img_path = model.get_image_paths()
+        print('process image... %s' % img_path)
+        visualizer.save_images(webpage, visuals, img_path)
+
+    webpage.save()
+
+if __name__ == '__main__':
+    main()
