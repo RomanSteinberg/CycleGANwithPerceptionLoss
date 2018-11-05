e9e4faeec4629ba8ea7574375357065c575c8ffa
Sergey Kastryulin <snk4tr@gmail.com>
2018-11-05 11:29:58 +0300

    feat(networks): switchable normalization

--- train.sh
+++ train.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-pip install visdom dominate
 
-python3 train.py --name 20 --dataroot ./input --model cycle_gan --lambda_feat_AfB 0.1 --lambda_feat_BfA 0.1 --no_dropout --batchSize 1 \
- --no_flip --resize_or_crop 'resize_and_crop' --max_dataset_size 7500 \
- --display_id 0 --print_freq 1000 --display_freq 7500
+python3 train.py \
+ --name 2 \
+ --dataroot ./input \
+ --model cycle_gan \
+ --no_dropout \
+ --padding_type "zero" \
+ --which_model_netG 'resnet_12blocks' \
+ --lambda_A 15.0 \
+ --lambda_feat_AfB 0.4 \
+ --lambda_feat_BfA 0.2 \
+ --lambda_feat_fBrecA 0.2 \
+ --identity 0.1 \
+ --skip_gen_connection \
+ --batchSize 1 \
+ --no_flip \
+ --resize_or_crop 'resize_and_crop' \
+ --max_dataset_size 1843 \
+ --display_id 0 \
+ --print_freq 500 \
+ --display_freq 1000 \
+ --data_description "1843\ segmented\ and\ scaled\ photos\ and\ not\ augmented\ avatars\ 256x256"
+
