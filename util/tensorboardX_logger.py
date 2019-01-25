b0aab95e79fc54c111a4d06bd9c9e9ff034a6d2b
Mikhail <m.ulyanov96@mail.ru>
2019-01-25 18:44:13 +0300

    add layers visualization

--- util/tensorboardX_logger.py
+++ util/tensorboardX_logger.py
@@ -5,31 +5,34 @@ from tensorboardX import SummaryWriter
 
 class TBLogger:
     def __init__(self, opt, *args):
-        self.writer = {}
+        self._writer = {}
         for name in args:
             log_dir = os.path.join(opt.checkpoints_dir, opt.name, 'tensorboardX', name)
-            self.writer[name] = SummaryWriter(log_dir=log_dir, filename_suffix=f'.{name}')
+            self._writer[name] = SummaryWriter(log_dir=log_dir, filename_suffix=f'.{name}')
+
         layout = {
             'losses': {'value losses': ['Multiline', [f'loss/{name}'for name in args]]}
         }
-        self.writer[args[0]].add_custom_scalars(layout)
+        self._writer[args[0]].add_custom_scalars(layout)
 
     def log_graph(self, model, network_name, input_net):
-        self.writer[network_name].add_graph(getattr(model, network_name), input_net)
+        self._writer[network_name].add_graph(getattr(model, network_name), input_net)
 
     def log_loss(self, model, network_name, iteration):
         error_key = network_name.split('t')[1]
         error = model.get_current_errors()[error_key]
-        self.writer[network_name].add_scalar(f'loss/{network_name}', error.data, global_step=iteration)
+        self._writer[network_name].add_scalar(f'loss/{network_name}', error.data, global_step=iteration)
 
     def log_gradients(self, model, network_name, iteration):
-        # network_gradients = []
-        for name, param in getattr(model, network_name).named_parameters():
+        i = 0
+        for name, param in list(getattr(model, network_name).named_parameters())[::-1]:
             split_name = name.split('.')
             if split_name[-1] == 'weight':
-                self.writer[network_name].add_histogram('.'.join(x for x in split_name[0:-1])+'.gradients', param.grad, iteration, bins='doane')
-                # network_gradients.append(param.grad.cpu().numpy())
+                name = '.'.join(x for x in split_name[0:-1]) + '.gradients'
+                self._writer[network_name].add_histogram(name, param.grad, iteration, bins='doane')
+                self._writer[network_name].add_histogram(f'hist/{network_name}_{iteration}', param.grad, global_step=i)
+                i += 1
 
     def __del__(self):
-        for key in self.writer:
-            self.writer[key].close()
+        for key in self._writer:
+            self._writer[key].close()
